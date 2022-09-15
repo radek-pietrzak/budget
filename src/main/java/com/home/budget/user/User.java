88135d01f2ca0e0f3dc4789758dd53cca4372d32
Radek Pietrzak <radekpietrzak102@gmail.com>
2022-09-15 14:54:42 +0200

    #13 env

---
+++ src/main/java/com/home/budget/user/User.java
@@ -0,0 +1,22 @@
+//package com.home.budget.user;
+//
+//import lombok.*;
+//
+//import javax.persistence.*;
+//import javax.validation.constraints.Email;
+//
+//@Builder
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//@Table(name = "users")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String userName;
+//    @Email
+//    private String email;
+//    private String password;
+//}
